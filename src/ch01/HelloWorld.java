c2d335ec0918128285a530f95532f5ffabd5194c
kimguen999 <>
2026-06-10 17:23:51 +0900

    단축키 및 테마 설정

---
+++ src/ch01/HelloWorld.java
@@ -0,0 +1,18 @@
+package ch01;
+
+// 2026.06.10 수
+// 이클립스 다운 및 설치, 실행
+// 단축키 세팅 및 테마 변경
+// print, println, 주석
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		// System.out.println("Hello World");
+		
+	}
+
+}
